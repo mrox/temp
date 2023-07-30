fd4cd867567a642bd79ee76fe966640933d69f85
tuyentran <tuyen.appdev@gmail.com>
2023-07-30 12:05:04 +0700

    add epaper sh

---
+++ epaper_cloudflare.sh
@@ -0,0 +1,5 @@
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
+
+sudo dpkg -i cloudflared.deb && 
+
+sudo cloudflared service install eyJhIjoiN2RlMTY4ZGVkYzQzNmEyMGEzZGY1MDQ3NGE5Y2I0ZWIiLCJ0IjoiMjcxZDk2ZGEtMWIzZS00NzNjLWJmMjctZjI0ZDFhNzc5ZjEzIiwicyI6IlpHSTBOV0poTW1FdE0yUTJNaTAwTW1aakxXSXpNek10WkRReU4yRmpabUpsTTJOaCJ9
